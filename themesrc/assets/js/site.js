cb6bf44cd7a15b698c6ca53310c9af8316c69649
Tim Borst <borst@zilverfolie.nl>
2022-12-07 22:01:17 +0100

    JS is goed, Seb moet even meekijken wat ik met die build heb gedaan :)

--- themesrc/assets/js/site.js
+++ themesrc/assets/js/site.js
@@ -27,82 +27,102 @@ const tabContent = document.getElementsByClassName('tab-content')
 const initApp = () => {
 
 	// tabs
-	hideTabsContent(1)
-	tab[0].classList.add('active')
-	tabContent[0].classList.add('show')
+	const ifTabs = document.getElementsByClassName('tabs');
+		if (ifTabs.length > 0) {
 
-	document.getElementById('tabs').onclick= function (event) {
-		var target=event.target;
-		if (target.className=='tab') {
-			for (var i=0; i<tab.length; i++) {
-				if (target == tab[i]) {
-					showTabsContent(i)
-					break;
+		hideTabsContent(1)
+		tab[0].classList.add('active')
+		tabContent[0].classList.add('show')
+
+		document.getElementById('tabs').onclick= function (event) {
+			var target=event.target;
+			if (target.className=='tab') {
+				for (var i=0; i<tab.length; i++) {
+					if (target == tab[i]) {
+						showTabsContent(i)
+						break;
+					}
 				}
+			}
+		}
+
+		function hideTabsContent(a) {
+			for (var i=a; i<tabContent.length; i++) {
+				tabContent[i].classList.remove('show')
+				tabContent[i].classList.add("hide")
+				tab[i].classList.remove('active')
+			}
+		}
+
+		function showTabsContent(b){
+			if (tabContent[b].classList.contains('hide')) {
+				hideTabsContent(0)
+				tab[b].classList.add('active')
+				tabContent[b].classList.remove('hide')
+				tabContent[b].classList.add('show')
 			}
 		}
 	}
 
-	function hideTabsContent(a) {
-		for (var i=a; i<tabContent.length; i++) {
-			tabContent[i].classList.remove('show')
-			tabContent[i].classList.add("hide")
-			tab[i].classList.remove('active')
-		}
-	}
-
-	function showTabsContent(b){
-		if (tabContent[b].classList.contains('hide')) {
-			hideTabsContent(0)
-			tab[b].classList.add('active')
-			tabContent[b].classList.remove('hide')
-			tabContent[b].classList.add('show')
-		}
-	}
-
 	// scroll element with GSAP
-	const benefits = document.querySelector('.benefits-slider .benefits')
-	const benefitsWidth = benefits.clientWidth - 1200
+	const ifStatsImage = document.getElementsByClassName('stats');
+		if (ifStatsImage.length > 0) {
 
-	gsap.to('.stats .image-holder img', {
-		y: 0,
-		scrollTrigger: {
-			trigger: '.stats',
-			start: 'top 180',
-			// end: 'center 50%',
-			scrub: true
-		}
-	})
+		gsap.to('.stats .image-holder img', {
+			y: 0,
+			scrollTrigger: {
+				trigger: '.stats',
+				start: 'top 180',
+				// end: 'center 50%',
+				scrub: true
+			}
+		})
+	}
 
-	gsap.to('.text-image-cta .image-holder img', {
-		y: 0,
-		scrollTrigger: {
-			trigger: '.text-image-cta',
-			start: 'top 50%',
-			// end: 'center 50%',
-			scrub: true
-		}
-	})
+	const ifTextImageCTA = document.getElementsByClassName('text-image-cta');
+		if (ifTextImageCTA.length > 0) {
 
-	gsap.to('.advertise .image-holder img', {
-		y: 0,
-		scrollTrigger: {
-			trigger: '.advertise',
-			start: 'top 50%',
-			// end: 'center 50%',
-			scrub: true
-		}
-	})
+		gsap.to('.text-image-cta .image-holder img', {
+			y: 0,
+			scrollTrigger: {
+				trigger: '.text-image-cta',
+				start: 'top 50%',
+				// end: 'center 50%',
+				scrub: true
+			}
+		})
+	}
 
-	gsap.to('.benefits-slider .benefits', {
-		x: - benefitsWidth,
-		scrollTrigger: {
-			trigger: '.benefits-slider .benefits',
-			start: 'top 25%',
-			end: 'bottom 50%',
-			scrub: 1
-		}
-	})
+	const ifAdvertiseImage = document.getElementsByClassName('advertise');
+		if (ifAdvertiseImage.length > 0) {
+
+		gsap.to('.advertise .image-holder img', {
+			y: 0,
+			scrollTrigger: {
+				trigger: '.advertise',
+				start: 'top 50%',
+				// end: 'center 50%',
+				scrub: true
+			}
+		})
+	}
+
+	const ifBenefitsSlider = document.getElementsByClassName('benefits-slider');
+		if (ifBenefitsSlider.length > 0) {
+
+		const benefits = document.querySelector('.benefits-slider .benefits')
+		const benefitsWidth = benefits.clientWidth - 1200
+
+		gsap.to('.benefits-slider .benefits', {
+			x: - benefitsWidth,
+			scrollTrigger: {
+				trigger: '.benefits-slider .benefits',
+				start: 'top 25%',
+				end: 'bottom 50%',
+				scrub: 1
+			}
+		})
+	}
+
 }
-
 document.addEventListener('DOMContentLoaded', initApp)
