86a0387a3ccec21eb9ab4f8570132fd9967c8db0
Seb van Dijk <seb@developr.nl>
2023-05-11 17:14:52 +0200

    Update header.php
    remove marker

--- themesrc/header.php
+++ themesrc/header.php
@@ -30,31 +30,7 @@
             rel="stylesheet"
             href="https://cdn.jsdelivr.net/npm/swiper/swiper-bundle.min.css"
     />
-    <script>
-        window.markerConfig = {
-            project: '63d2347bd3d532a8f8336f00',
-            source: 'snippet'
-        };
-    </script>
-
-    <script>
-        !function (e, r, a) {
-            if (!e.__Marker) {
-                e.__Marker = {};
-                var t = [], n = {__cs: t};
-                ["show", "hide", "isVisible", "capture", "cancelCapture", "unload", "reload", "isExtensionInstalled", "setReporter", "setCustomData", "on", "off"].forEach(function (e) {
-                    n[e] = function () {
-                        var r = Array.prototype.slice.call(arguments);
-                        r.unshift(e), t.push(r)
-                    }
-                }), e.Marker = n;
-                var s = r.createElement("script");
-                s.async = 1, s.src = "https://edge.marker.io/latest/shim.js";
-                var i = r.getElementsByTagName("script")[0];
-                i.parentNode.insertBefore(s, i)
-            }
-        }(window, document);
-    </script>
+  
 	
 	
 	<?php wp_head(); ?>
