10188bf55b114811796e39cb973b69b3321e6e49
Seb van Dijk <seb@developr.nl>
2023-06-07 09:52:11 +0200

    Update header.php

--- themesrc/header.php
+++ themesrc/header.php
@@ -31,7 +31,10 @@
             href="https://cdn.jsdelivr.net/npm/swiper/swiper-bundle.min.css"
     />
    
-	
+	<!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-MJWMXXF');</script>
+<!-- End Google Tag Manager -->
+
 	<?php wp_head(); ?>
 </head>
 <body <?php body_class( ! is_front_page() ? 'subpage' : '' ); ?>>
