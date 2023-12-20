0504023d724204f2e48245128d3dc8980b9c22a8
Seb <seb@developr.nl>
2023-12-20 09:43:57 +0100

    fix buttons

--- themesrc/template-parts/block-other_brands.php
+++ themesrc/template-parts/block-other_brands.php
@@ -6,34 +6,36 @@ if ( isset( $args ) ) {
 	$fields = $args;
 }
 $api_url = $fields['api_url'];
-$logos   = get_transient( 'pmmm_brand-logos-' . md5( $api_url ) );
-if ( false === $logos ) {
+if ( $api_url != '' ) {
 	
-	
-	$response = wp_remote_get( $api_url );
-	if ( is_wp_error( $response ) ) {
-		return false;
+	$logos = get_transient( 'pmmm_brand-logos-' . md5( $api_url ) );
+	if ( false === $logos ) {
+		
+		
+		$response = wp_remote_get( $api_url );
+		if ( is_wp_error( $response ) ) {
+			return false;
+		}
+		
+		$logos = json_decode( wp_remote_retrieve_body( $response ), true );
+		$logos = explode( ';', $logos );
+		
+		set_transient( 'pmmm_brand-logos-' . md5( $api_url ), $logos, 12 * HOUR_IN_SECONDS );
 	}
 	
-	$logos = json_decode( wp_remote_retrieve_body( $response ), true );
-	$logos = explode( ';', $logos );
 	
-	set_transient( 'pmmm_brand-logos-' . md5( $api_url ), $logos, 12 * HOUR_IN_SECONDS );
-}
-
-
-if ( isset( $logos ) && is_array( $logos ) ) {
-	$brand_logos = '<div class="marquee">';
-	foreach ( $logos as $key => $logo ) {
-		if ( $logo != '' ) {
-			$brand_logos .= '<figure><img decoding="async" loading="lazy"
+	if ( isset( $logos ) && is_array( $logos ) ) {
+		$brand_logos = '<div class="marquee">';
+		foreach ( $logos as $key => $logo ) {
+			if ( $logo != '' ) {
+				$brand_logos .= '<figure><img decoding="async" loading="lazy"
                                     src="' . $logo . '"/>
                             </figure>';
+			}
 		}
+		$brand_logos .= '</div>';
 	}
-	$brand_logos .= '</div>';
 }
-
 ?>
 
 <!-- other brands -->
@@ -41,15 +43,30 @@ if ( isset( $logos ) && is_array( $logos ) ) {
     <div class="inner centered">
         <h6><?= $fields['label'] ?></h6>
         <h2><?= $fields['title'] ?></h2>
-        <div class="content">
+		<?= $fields['content'] ?>
+    </div>
 
-            <div class="marquee-full-width">
-                <div class="marquee-box">
-					<?= $brand_logos ?>
+    <div class="inner">
+		<?php if ( is_array( $fields['brands'] ) ) { ?>
+            <div class="brands row">
+				<?php foreach ( $fields['brands'] as $brand ) {
+					$brand['brand_cta']['icon'] = 'arrow'; ?>
+                    <div class="brand">
+                        <h5><?= $brand['brand_title'] ?></h5>
+                        <div class="image-holder"><?= wp_get_attachment_image( $brand['brand_logo'], 'small' ) ?></div>
+						<?= button( $brand['brand_cta'], 'brand' ) ?>
+                    </div>
+				<?php } ?>
+				<?php if ( isset( $brand_logos ) && $brand_logos != '' ) { ?>
+                    <div class="marquee-full-width">
+                        <div class="marquee-box">
+							<?= $brand_logos ?>
 
-                </div>
+                        </div>
+                    </div>
+				<?php } ?>
             </div>
-        </div>
+		<?php } ?>
     </div>
 
 
